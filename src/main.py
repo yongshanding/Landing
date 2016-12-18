d0a07fe1220adb52c9605826b2735b88fb3fca1c
Yongshan Ding <yongshanding@users.noreply.github.com>
2016-12-17 22:35:34 -0600

    Initial setup

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import pygame
+from sys import exit
+from datetime import datetime
+
+def main():
+    print "Starting Landing game..."
+    print "Landing game ends."
+
+if __name__ == '__main__':
+    main()
+
---
+++ tests/test.py
@@ -0,0 +1,12 @@
+# Class of testing suite
+
+# import game lib here
+
+import unittest
+
+class LocalTestSuite(unittest.TestCase):
+    def foo():
+        return 42
+
+if __name__ == '__main__':
+    unittest.main()
